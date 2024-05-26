ced3afd1ad949336fd32c67333cc7e96cb9f8ad5
koukisaifedinn <koukisaif1996@gmail.com>
2024-05-26 16:39:58 +0100

    spring

---
+++ week 2/Fruity/src/main/java/com/saif/fruity/FruityApplication.java
@@ -0,0 +1,15 @@
+package com.saif.fruity;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruityApplication.class, args);
+	}
+
+}
